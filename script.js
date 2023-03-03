dddd3347f071ec462ba07c8b80f51131ca7c20fa
[Jaypa92] <j.parris1@aol.com>
2023-03-02 21:35:02 -0500

    update

---
+++ script.js
@@ -0,0 +1,8 @@
+function change(element) {
+    element.src = "./119181751_2661708150735740_926792687261007363_n-2.jpg"
+}
+
+function changePic(id) {
+    let pic = document.querySelector('#bod');
+    pic.background = "after_idea-3.jpg"
+}
